fd51157985db331704e7e84a9db2df8d285622ff
Taiye Kadiri <taiyefunmilola@gmail.com>
2023-01-24 10:18:28 +0100

    added scripts to change navbar effect

--- js/script.js
+++ js/script.js
@@ -1,83 +1,37 @@
-// Grab elements
-const selectElement = selector => {
-    const element = document.querySelector(selector)
-    if(element) return element;
-    throw new Error(`Something went wrong, make sure that ${selector} exists or is typed correctly.`);
-};
-
-// Nav style on scroll
-const scrollHeader = () => {
-    const headerElement = selectElement('#header');
-    if(this.scrollY >= 15){
-        headerElement.classList.add('activated');
-    }else{
-        headerElement.classList.remove('activated');
-    }
-};
-
-window.addEventListener('scroll', scrollHeader);
-
-// open menu & search pop-up
-const menuToggleIcon = selectElement('#menu-toggle-icon');
-
-const toggleMenu = () => {
-    const mobileMenu = selectElement('#menu');
-    mobileMenu.classList.toggle('activated');
-    menuToggleIcon.classList.toggle('activated');
-};
-
-menuToggleIcon.addEventListener('click', toggleMenu);
-
-// // open/close search from pop-up
-// const formOpenBtn = selectElement('#search-icon');
-// const formCloseBtn = selectElement('#form-close-btn');
-// const searchFormContainer = selectElement('#search-form-container');
-
-// formOpenBtn.addEventListener('click', () => searchFormContainer.classList.add('activated'));
-
-// formCloseBtn.addEventListener('click', () => searchFormContainer.classList.remove('activated'));
-
-// // --close the search from pop-up on esc keypress
-// window.addEventListener('keyup', event => {
-//     if(event.key === 'Escape') searchFormContainer.classList.remove('activated')
-// });
-
-// switch theme/add to local storage
-const bodyElement = document.body;
-const themeToggleBtn = selectElement('#color-theme-toggle-btn');
-const currentTheme = localStorage.getItem('currentTheme');
-
-if(currentTheme){
-    bodyElement.classList.add('light-theme');
-}
-
-themeToggleBtn.addEventListener('click', () => {
-    bodyElement.classList.toggle('light-theme');
-
-    if(bodyElement.classList.contains('light-theme')){
-        localStorage.setItem('currentTheme','themeActive');
-    }else{
-        localStorage.removeItem('currentTheme');
+let navbarDiv = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+    if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40){
+        navbarDiv.classList.add('navbar-cng');
+    } else {
+        navbarDiv.classList.remove('navbar-cng');
     }
 });
 
-// swiper
-const swiper = new Swiper('.swiper',{
-    slidesPerView: 1,
-    spaceBetween: 20,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    pagination: {
-        el: '.swiper-pagination'
-    },
-    breakpoints: {
-        700: {
-            slidesPerView: 2
-        },
-        1200: {
-            slidesPerView: 3
-        }
+const navbarCollapseDiv = document.getElementById('navbar-collapse');
+const navbarShowBtn = document.getElementById('navbar-show-btn');
+const navbarCloseBtn = document.getElementById('navbar-close-btn');
+// show navbar
+navbarShowBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.add('navbar-collapse-rmw');
+});
+
+// hide side bar
+navbarCloseBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target.id != "navbar-collapse" && e.target.id != "navbar-show-btn" && e.target.parentElement.id != "navbar-show-btn"){
+        navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
     }
+});
+
+// stop transition and animatino during window resizing
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
+    }, 400);
 });
